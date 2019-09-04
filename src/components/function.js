cfd062a5cc7ab99489efe6b822efb6d6d582a838
Johannes Reker <j.reker@ktr.com>
2019-09-04 10:14:51 +0200

    ended tut v5

--- src/components/function.js
+++ src/components/function.js
@@ -1,22 +1,49 @@
 import React from 'react';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 
 //change state
 const reducer = function(state, action) {
     if(action.type === "INC") {
         return state+action.payload;
     }
-    if(action.type === "DEC") {
+    else if(action.type === "DEC") {
         return state-action.payload;
+    } else if(action.type=== "E") {
+        throw new Error("Test");
     }
-
 
     return state;
 }
+//------------------------------
 
-const store = createStore(reducer, 0);
+//Middleware runs between every action
+const logger = (store) => (next) => (action) => {
+    console.log("action fired", action);
+    //we can also modify the action if we want to
+    //action.type = "DEC";
+    //no action will be run till we call it via next
+    next(action);
+};
+
+const error = (store) => (next) => (action) => {
+    console.log("action fired", action);
+
+    try{
+        next(action);
+    } catch(e) {
+        console.log("HELP!!",e);
+    }
+    
+};
+
+
+
+const middleware = applyMiddleware(logger, error);
+
+const store = createStore(reducer, 0, middleware);
+
 store.subscribe(() => {
-    console.log("Store changed", store.getState())
+    console.log("Store Changed", store.getState());
 })
 
 store.dispatch({type: "INC", payload: 1});
@@ -25,6 +52,7 @@ store.dispatch({type: "INC", payload: 3});
 store.dispatch({type: "INC", payload: 4});
 store.dispatch({type: "INC", payload: 1});
 store.dispatch({type: "DEC", payload: 1});
+store.dispatch({type: "E", payload: 1});
 
 
 
